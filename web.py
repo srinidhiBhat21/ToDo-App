31b90627d2adb7f2c1ce00db84e8911ee4a1edfd
srinidhiBhat21 <srinidhi4bhat@gmail.com>
2026-06-18 21:09:38 +0530

    Implemented checkbox pop func

--- web.py
+++ web.py
@@ -13,8 +13,13 @@ st.title("My To-Do App")
 st.subheader("This is my to-do app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox= st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 st.text_input(label="Enter a to-do item", 
               placeholder="Type a to-do", on_change=add_todo, 
